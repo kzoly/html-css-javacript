d5e1a3afbfee02f1b1764c6b1d9a087b6f226076
kzoly <78027613+kzoly@users.noreply.github.com>
2021-07-26 23:05:35 +0300

    homework

--- js/employees.js
+++ js/employees.js
@@ -9,3 +9,68 @@ function showHideSalary() {
     const hiddenSalaries = document.getElementsByClassName('hideSalary');    
     showHideButton.innerText = hiddenSalaries.length ? 'Show Salary' :'Hide Salary';
 }
+const employee=[];
+function addelement()
+{ 
+    if(document.getElementById("employeename").value=='')
+      
+      alert("employe name is empty");
+    else 
+      { employee[employee.length]=document.getElementById("employeename").value;
+       
+      }
+      //console.log(employee);
+      document.getElementById("inputfield").innerHTML =employee;
+}
+
+
+function addelement2()
+{ 
+    if(document.getElementById("employeename2").value=='')
+      
+      alert("employe name is empty");
+    else 
+      { employee[employee.length]=document.getElementById("employeename2").value;
+       
+      }
+      //console.log(employee);
+      buildtable(employee);
+}
+
+function clearinput()
+{
+    document.getElementById('employeename').value = ''
+}
+
+function buildtable(data){
+    var tableRef = document.getElementById('tableId');
+    while ( tableRef.rows.length > 0 )
+    {
+     tableRef.deleteRow(0);
+    
+   }
+
+        var table=document.getElementById('tblbody')
+    for(var i=0;i<data.length;i++)
+    {
+        var row = `<tr>
+							<td>${data[i]}</td>
+							<td> <button onclick="deleteelement(${i})">Delete</button></td>
+					  </tr>`
+             table.innerHTML+=row;    
+    }
+}
+function deleteelement(i){
+employee.splice(i,1);
+buildtable(employee);
+}
+
+function jsonstringify()
+{
+    const obj={name:document.getElementById("fname").value,
+               function:document.getElementById("ffunction").value,
+               salary:document.getElementById("fsalary").value
+            };
+    const myjson=JSON.stringify(obj);
+    alert(myjson);
+}
